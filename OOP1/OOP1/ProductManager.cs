c3ba0aba6c422f3f5b6c2bc2c3790c438af11bad
Pelin Altürk <80326351+pelinalturk@users.noreply.github.com>
2021-11-13 15:55:22 +0300

    Add files via upload

---
+++ OOP1/OOP1/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün Eklendi: "+ product.ProductName);
+        }
+    }
+}
---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product {Id=1,CategoryId= 2,ProductName= "Masa" , UnitInStock=3, UnitPrice=500};
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
