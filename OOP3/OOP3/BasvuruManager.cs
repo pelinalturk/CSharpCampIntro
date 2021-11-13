e7a5cd6a9ea3b034f283aa62949745d29084b572
Pelin Altürk <80326351+pelinalturk@users.noreply.github.com>
2021-11-13 15:56:21 +0300

    Add files via upload

---
+++ OOP3/OOP3/BasvuruManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(ICrediManager crediManager, List<ILoggerService> loggerServices)
+        {
+            crediManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<ICrediManager> credis)
+        {
+            foreach (var credi in credis)
+            {
+                credi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/OOP3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new List<ILoggerService> { new DataBaseLoggerService(), new FileLoggerService()});
+
+            List<ICrediManager> credis = new List<ICrediManager>() {ihtiyacKrediManager };
+
+           // basvuruManager.KrediOnBilgilendirmesiYap(credis);
+        }
+    }
+}
