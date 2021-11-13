ec0a5952bb7a9934e316d40b7f50ac731fcb71af
Pelin Altürk <80326351+pelinalturk@users.noreply.github.com>
2021-11-13 15:50:50 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> nameAndId = new Dictionary<int, string> ();
+            nameAndId.Add(0, "Pelin");
+            nameAndId.Add(1, "Engin");
+
+            _MyDictionary<int, string> s = new _MyDictionary<int, string>();
+            s.Add(1, "");
+            s.Add(1, "");
+            Console.WriteLine(s.Count());
+        }
+    }
+}
---
+++ MyDictionary/MyDictionary/_MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class _MyDictionary<T,F>
+    {
+        T[] tItem;
+        F[] fItem;
+        public _MyDictionary()
+        {
+            tItem = new T[0];
+            fItem = new F[0];
+        }
+        public void Add(T TKey, F TValue)
+        {
+            T[] tempArray = tItem;
+            F[] _tempArray = fItem;
+            tItem = new T[tItem.Length + 1];
+            fItem = new F[fItem.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                tItem[i] = tempArray[i];
+                fItem[i] = _tempArray[i];
+            }
+            tItem[tItem.Length - 1] = TKey;
+            fItem[fItem.Length - 1] = TValue;
+        }
+        public int Count()
+        {
+            return tItem.Length;
+        }
+    }
+}
