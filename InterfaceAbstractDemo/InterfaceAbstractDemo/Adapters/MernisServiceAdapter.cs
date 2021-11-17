318c10e87a6fc71b08369c7a829784cbd958284a
Pelin Altürk <80326351+pelinalturk@users.noreply.github.com>
2021-11-17 11:59:56 +0300

    Add files via upload

---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : IPersonCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(
+            KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(),
+            customer.LastName.ToUpper(), customer.DateOfBirth.Year).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+
+namespace InterfaceAbstractDemo
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager baseCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            baseCustomerManager.Save(new Entities.Customer { DateOfBirth= new System.DateTime(1985,1,6), FirstName="Engin", LastName="Demiroğ", NationalityId="28861499108"} );            
+        }
+    }
+}
